1a560d590db3a3f77587a828f5d3946739d9654f
TheAenema <thegreatnone1995@gmail.com>
2025-02-12 18:26:31 +0330

    Porting to Shell

--- test/vswhere.test/TestHelper.cpp
+++
@@ -1,8 +0,0 @@
-// <copyright file="TestHelper.cpp" company="Microsoft Corporation">
-// Copyright (C) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license. See LICENSE.txt in the project root for license information.
-// </copyright>
-
-#include "stdafx.h"
-
-const ci_equal TestHelper::s_comparer;
--- test/vswhere.test/stdafx.cpp
+++
@@ -1,6 +0,0 @@
-// <copyright file="stdafx.cpp" company="Microsoft Corporation">
-// Copyright (C) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license. See LICENSE.txt in the project root for license information.
-// </copyright>
-
-#include "stdafx.h"
